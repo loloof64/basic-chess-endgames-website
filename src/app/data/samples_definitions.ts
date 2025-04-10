dfeaf4f521f1e1f84e43141095226977c70e3428
laurent bernabé <laurent.bernabe@gmail.com>
2025-04-10 09:54:02 +0200

    Added two sample scripts for download

--- src/app/data/samples_definitions.ts
+++ src/app/data/samples_definitions.ts
@@ -7,4 +7,12 @@ export const additionalSamples = [
     captionKey: "nbk_k",
     path: "/scripts/nbk_k.txt",
   },
+  {
+    captionKey: "qk_pk",
+    path: "/scripts/qk_pk.txt",
+  },
+  {
+    captionKey: "rk_bk",
+    path: "/scripts/rk_bk.txt",
+  },
 ];
